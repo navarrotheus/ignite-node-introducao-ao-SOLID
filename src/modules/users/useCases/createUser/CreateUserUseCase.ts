dd98a998e7475ab477bde7082a841120c1498d81
Matheus Navarro <matheus.navarro@meliuz.com.br>
2021-04-23 19:53:47 -0300

    finish challenge 01 - chapter II

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userWithEmail = this.usersRepository.findByEmail(email);
+
+    if (userWithEmail) {
+      throw new Error("User with email already exists.");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
